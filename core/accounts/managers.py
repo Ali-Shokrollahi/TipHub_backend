ffb7be6475e34b625abd4c396e85e9edb13c76d0
Ali-Shokrollahi <ali.shokrollahi.pro@gmail.com>
2022-09-19 15:13:19 +0430

    make migrations for accounts and profiles apps.

--- core/accounts/managers.py
+++ core/accounts/managers.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
-from accounts.models import User
-
 
 class UserManager(BaseUserManager):
     """
