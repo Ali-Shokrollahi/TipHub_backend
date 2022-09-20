07c5ddcb50ade920d4f1a0bf5b8a501e8673cd55
Ali-Shokrollahi <ali.shokrollahi.pro@gmail.com>
2022-09-20 12:44:57 +0430

    add profile models admin interface.

--- core/profiles/admin.py
+++ core/profiles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import StudentProfile, TeacherProfile, StaffProfile
 
-# Register your models here.
+
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+class TeacherProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'username']
+
+
+class StaffProfileAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+
+admin.site.register(StudentProfile, StudentProfileAdmin)
+admin.site.register(TeacherProfile, TeacherProfileAdmin)
+admin.site.register(StaffProfile, StaffProfileAdmin)
