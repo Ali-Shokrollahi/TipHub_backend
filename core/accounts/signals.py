d5821c67d63d58dfd7911c6c7ace3a78d7760959
Ali-Shokrollahi <ali.shokrollahi.pro@gmail.com>
2022-09-20 12:44:41 +0430

    add signal for creating user profile.

---
+++ core/accounts/signals.py
@@ -0,0 +1,19 @@
+from .models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from profiles.models import StudentProfile, TeacherProfile, StaffProfile
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        if instance.role == "STU":
+            StudentProfile.objects.create(user=instance)
+
+        elif instance.role == "TEA":
+            TeacherProfile.objects.create(user=instance)
+
+        elif instance.role == "STA":
+            StaffProfile.objects.create(user=instance)
+        else:
+            pass
